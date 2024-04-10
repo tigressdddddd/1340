e8f8bede8c2e5fb564d393d922ee730adeb45ba2
Charlice3612826 <146347928+Charlice3612826@users.noreply.github.com>
2024-04-10 21:07:18 +0800

    Update gobang.h

--- gobang.h
+++ gobang.h
@@ -28,36 +28,36 @@ extern int Board[SIZE][SIZE];       	// chessboard
 extern int ScoreBoard_b[SIZE][SIZE];	// scoreboard for black piece
 extern int ScoreBoard_w[SIZE][SIZE];	// scoreboard for white piece 
 
-extern int mode; 						//0为人人对战，1为人机对战 
-extern int side_p, side_ai;			// 记录选边情况 
-extern int Posi[2];					// 记录落子位置 
-extern int LineArray[9];				// 记录单一方向的落子情况 
-extern int LineStateRecord[9];			// 记录某一位置可能形成的棋型，索引对应着棋型（见宏） 
+extern int mode; 						//mode 0: man vs man, mode 1: man vs machine  
+extern int side_p, side_ai;			// record which side you choose 
+extern int Posi[2];					// record the place where you drop the chess piece
+extern int LineArray[9];				// record the dropping chess of a single side  
+extern int LineStateRecord[9];			// Records possible moves in a position, with indexes corresponding to moves 
 
 //display
 void DisplayBoard();
 
 //judge
-int judge(int x, int y); 		// 裁判 ，判断胜负和禁手 
-void ChooseMode(); 				// 人人或人机 
-void ChooseSide();  			// 选边 
-int RestrictedMove(int x, int y); // 禁手判定 
+int judge(int x, int y); 		// judge winning and restricted move 
+void ChooseMode(); 				// choose mode
+void ChooseSide();  			// choose edge
+int RestrictedMove(int x, int y); // determine if it is a restricted move
 
 //move
-void MoveP(int *pBoard, int piece);	// 人落子 
-void MoveAi(int piece); 			//ai落子 
+void MoveP(int *pBoard, int piece);	// player move
+void MoveAi(int piece); 			//ai move
 
 //score
-void ChangeScoreBoard(int x, int y, int (*pBoard)[15]); 		// 更新分数板 
-void ScorePosi(int x, int y, int (*pBoard)[15], int side); 	//对特定位置打分 
-int LineState(int *pLineArray); 						//输出一行中最强的棋型 
-void ChangeLine(int *pLineArray); 						//将LineArray处理成需要的形式 
+void ChangeScoreBoard(int x, int y, int (*pBoard)[15]); 		// refresh the scoreboard
+void ScorePosi(int x, int y, int (*pBoard)[15], int side); 	//score specific position 
+int LineState(int *pLineArray); 						//output the most powerful chess type 
+void ChangeLine(int *pLineArray); 						//turn LineArray into the needed format
 
 
 //board_array
-void InitBoardArray();  			//初始化空棋盘
-void ChangeTemPieces(int piece); 	//将tem棋子转化为正常棋子 
-void PrintBoardArray(int *pBoard); 	//用于测试 
+void InitBoardArray();  			//initialize empty chessboard
+void ChangeTemPieces(int piece); 	//turn tem chess piece to normal chess piece 
+void PrintBoardArray(int *pBoard); 	//for test
 
 
 
